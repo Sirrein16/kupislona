19a3d2574cfefd4246cd3ba1f4a0ee3ef4297091
Sirrein16 <166790549+Sirrein16@users.noreply.github.com>
2024-04-19 11:21:34 +0500

    Update solanka.py

--- solanka.py
+++ solanka.py
@@ -1,1 +1,23 @@
+from pygame import*
 
+okno = display.set_mode((1000,650))
+fps = time.Clock()
+game = True
+
+class gameobject(sprite.Sprite):
+    def __init__(self, img, x,y,w,h):
+        super().__init__()
+        self.image = transform.scale(image.load(img), (w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.lastx = self.rect.x
+        self.lasty = self.rect.y
+    def ris(self):
+        okno.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+    fps.tick(60)
+    display.update()
+ 
